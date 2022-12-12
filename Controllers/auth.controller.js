01a8a401fc41f3a514e40fce9b3edd54fc67c869
hem193 <hemanthcastel@gmail.com>
2022-12-12 12:57:04 +0530

    AuthController - login,logout

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -45,3 +45,62 @@ export const signUp = asyncHandler(async (req, res) => {
     user,
   });
 });
+
+/*
+
+@SIGNIN
+@route hhtp://localhost:5000/api/auth/signup
+@description User signin controller for logging a new user
+@parameters  email,password
+@returns User Object
+
+ */
+
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomError("Please fill all fields", 400);
+  }
+  const user = User.findOne({ email }).select("+password");
+
+  if (!user) {
+    throw new CustomError("Invalid credentials", 400);
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (isPasswordMatched) {
+    const token = user.getJwtToken();
+    user.password = undefined;
+    res.cookie("token", token, cookieOptions);
+
+    res.status(200).json({
+      success: true,
+      token,
+      user,
+    });
+  }
+
+  throw new CustomError("Invalid credentials - pass", 400);
+});
+
+/*
+@LOGOUT
+@route hhtp://localhost:5000/api/auth/logout
+@description User logout by clearing user cookies
+@parameters  
+@returns success message
+ */
+
+export const logout = asyncHandler(async (_req, res) => {
+  //res.clearCookie()
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
+});
